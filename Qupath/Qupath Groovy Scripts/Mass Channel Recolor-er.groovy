318006040fe706e338e031aa62eb1b24282b7984
rhannan011 <70218799+rhannan011@users.noreply.github.com>
2025-07-23 14:35:29 -0400

    Update Mass Channel Recolor-er.groovy

--- Qupath/Qupath Groovy Scripts/Mass Channel Recolor-er.groovy
+++ Qupath/Qupath Groovy Scripts/Mass Channel Recolor-er.groovy
@@ -5,7 +5,7 @@
  * @author Riley Hannan
  * 
  * todo: build a script which can take a nicely colored multichannel image, 
- * export individual channels as GREY (alternatively export a high res multichannel viewer image?), 
+ * export individual channels as WHITE (alternatively export a high res multichannel viewer image?), 
  * and then restore or otherwise return the image to its original polychromatic state.
  * 
  * maybe using the generating/saving/loading of preset settings seen in the Channels class https://github.com/BIOP/qupath-extension-biop/releases/tag/v1.1.0
